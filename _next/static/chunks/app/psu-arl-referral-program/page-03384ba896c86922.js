4f721d9c9eb61953455f7bf5fe34410b9253aacb
cbmagent <cbmagent@openclaw>
2026-04-26 21:35:03 -0400

    fix: add waitForLoadState(networkidle) before title check to ensure all JS loaded

---
+++ _next/static/chunks/app/psu-arl-referral-program/page-03384ba896c86922.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8244],{751:()=>{},5723:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,751,23))}},e=>{e.O(0,[8476,2619,8441,1255,7358],()=>e(e.s=5723)),_N_E=e.O()}]);
